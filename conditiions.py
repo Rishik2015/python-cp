022fd8e8f43aec763ece09153acda6cf1cb889b1
Rishik2015 <panchalminu@gmail.com>
2025-08-02 10:58:27 +0530

    initial commit

---
+++ conditiions.py
@@ -0,0 +1,30 @@
+#Write a Python program to first take a number as an input from the user, then check whether that number is positive, negative, or neutral (if equal to zero) and print the result.
+# num=int(input("enter number: "))
+# if num>0:
+#     print("positive")
+# elif num<0:
+#     print("negative")   
+# elif num==0:
+#     print('neautral')
+# else:
+#     print("invalid input")    
+#LISTS
+
+fruits=["apple","banana"]
+print(fruits)
+     
+stu1=["rishik",5,5.3,True]
+
+
+#OPTRERATIONS
+print(len(stu1))
+
+fruits.append("orange")
+
+print(stu1[2])
+
+fruits.insert(1,"kiwi")
+print(fruits)
+
+fruits.pop()
+print(fruits)
