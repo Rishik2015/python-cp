9983498ffbfffd3e39d6e6da54a6c3e202128b88
Rishik2015 <panchalminu@gmail.com>
2025-08-06 16:54:13 +0530

    initial commit

---
+++ loops.py
@@ -0,0 +1,9 @@
+# num=int(input("write which number you want to print "))
+# for i in range(1,11):
+#     print(num,"x",i,"= ",num*i)
+    # Write a Python program for printing the sum of the first ten natural numbers using the while loop.
+
+sum=0
+for i in range(1,11):
+    sum= sum+i
+print("sum value :" ,sum)
