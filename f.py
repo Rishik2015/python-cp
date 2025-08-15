5bf51d1f4da2e59bbc8c4d878851a43d5f2c5cfa
Rishik2015 <panchalminu@gmail.com>
2025-08-15 18:47:34 +0530

    initial commit

---
+++ f.py
@@ -0,0 +1,19 @@
+# def greet():
+    # print("hello haw aree youii ")
+# def greet(name):
+     
+    #  print("hello", name, "how are you")
+
+# greet("rishik")
+# greet("kshitij")
+
+def abs(num):
+
+    if num>0:
+          return num
+    else:
+        
+         return -num
+
+n=int(input("enter a number:"))
+print(abs(n))
