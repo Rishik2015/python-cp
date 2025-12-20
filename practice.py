77c825c7e83da26c72f0faf064566eec1d7bf2be
Rishik2015 <panchalminu@gmail.com>
2025-12-20 10:46:50 +0530

    initial commit

---
+++ practice.py
@@ -0,0 +1,23 @@
+# wap to take input of 2 numbers and perform mulitplication
+# num1= int(input("enter your first number"))
+# num2= int(input("enter your second number"))
+
+# res= num1*num2
+# print("your answer is",res)
+
+# #wap to display the numbers which are divisible by 3 from the given list
+
+# l=[42,6,25,19,21,95,52]
+# i
+# for i in li:
+#     if i%3==0:
+#         print(i)
+
+
+# swap
+r= 10
+s= 330
+print("beore swapping")
+r,s=s,r
+print("r:",r,"s:",s)
+print("after swapping")
