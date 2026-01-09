24010520f004aca7f4a744a6308b4b75ed77dd97
Rishik2015 <panchalminu@gmail.com>
2026-01-09 18:46:55 +0530

    initial commit

---
+++ kw.py
@@ -0,0 +1,22 @@
+ # import keyword
+ # print("the python keywords")
+ # print(keyword.kwlist)
+
+# Python Program to Convert Celsius To Fahrenheit
+
+# temp=float(input("Enter a temparature in°C   "))
+# f= (temp*1.8)+32
+# print("the temperature in fahreinheit is",f)
+
+# WAP to find the greatest of the three numbers
+
+num1=int(input("enter 1st number"))
+num2=int(input("enter 2nd number"))
+num3=int(input("enter 3rd number"))
+
+if num1>num2 and num1>num3:
+    print("num1 is the greatest")
+elif num2>num1 and num2>num3:
+    print("num2 is the greatest") 
+else :
+    print("num3 is the greatest")       
