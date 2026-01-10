d9c63a31b8e21cee27ce9c93f26cd4e34eb21833
Rishik2015 <panchalminu@gmail.com>
2026-01-10 10:44:17 +0530

    initial commit

---
+++ datatype.py
@@ -0,0 +1,16 @@
+# Write a program to create, concatenate and print a string and accessing sub-string from a given string
+
+string1=input("enter your 1st string ")
+string2=input("enter your 2nd string ")
+
+print("the result is:  ",string1 + string2)
+
+#wap to count the occurrences of even and odd number in the given list
+
+li=[12,52,97,63,46,35,22,3,94]
+
+for i in range:
+    if%2:
+        print(i)
+    
+            
