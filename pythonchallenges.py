11ca482bbfc14f29372051b90368d295dbd8fd0c
Rishik2015 <panchalminu@gmail.com>
2026-02-14 10:45:43 +0530

    initial commit

---
+++ pythonchallenges.py
@@ -0,0 +1,30 @@
+# numerator=int(input("enter your numerator"))
+# denominator=int(input("enter your denominator"))
+
+# if (numerator%denominator)==0:
+#     print("your numbers are exactly divisible")
+# else:
+#     print("your numbers are not exactly divisible")    
+
+# mean1= 38
+# wrong_number=36
+# right_number=56
+# total=40
+
+# sum= mean1*40
+# print("the sum is",sum)
+# answer=total%sum
+# print("the answer is",answer)
+
+r=67
+i=77
+a=45
+n=18
+s=7
+h=100000
+i=23
+r=0
+
+r=(r+i)*h/n-s
+
+print(r)
