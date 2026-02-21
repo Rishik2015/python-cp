8bff31866aef7a47a7631a5e87369fac992cae1b
Rishik2015 <panchalminu@gmail.com>
2026-02-21 10:44:45 +0530

    initial commit

---
+++ forloop.py
@@ -0,0 +1,20 @@
+# Write a program to calculate the sum of whole numbers.
+
+# enter=int(input("enter the  number till you want to find the sum?"))
+# sum=0
+# for i in range(0,enter+1):
+#     sum=sum+i
+# print("the sum is: ",sum)
+
+
+# Write a program to reverse the string entered by the user.
+
+
+str1=input("enter a string:")
+str2=""
+
+for i in str1:
+    str2=i+str2
+
+print("original string :  ",str1)    
+print("reversed string  :",str2) 
