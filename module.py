2babff4a99415593c32319c4679a7f91ec9b557f
Rishik2015 <panchalminu@gmail.com>
2026-03-20 18:50:45 +0530

    initial commit

---
+++ module.py
@@ -0,0 +1,29 @@
+# Write a program to generate a random integer and match it with the input given by the user?
+
+# import random
+# playing = True
+# number = str(random.randint(0,9))
+# print("I will generate a number from 0 to 9 and you will have to guess with one number at a time")
+# print("The game ends when you get one hero")
+# while playing:
+#     guess = input("Give me your best guess ")
+#     if number == guess:
+#         print("you win the game")
+#         print("The number was ",number) 
+#         break   
+    
+#     else:
+#         print("YOU ARE VERY VERY MUCH WRONG.TRY AGAIN")  
+# 
+
+import math
+print("the floor and ceiling values of 31.33 are :" +  str(math.ceil(31.33)) + "," + str(math.floor(31.33)))  
+
+x= 10
+y= -15
+
+print("the vALUE  of x after copying the sign from y is :" + str(math.copysign(x,y)))
+
+print("absolute value of -96 and56 is:" + str(math.fabs(-96)) + "," + str(math.fabs(56)))
+print("Gcd is"+str(math.gcd(23,56)))
+
