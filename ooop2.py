a7836c427c94eb1fe45698adc0e81bc54a7022cc
Rishik2015 <panchalminu@gmail.com>
2026-04-24 18:42:31 +0530

    initial commit

---
+++ ooop2.py
@@ -0,0 +1,28 @@
+# class Iostring():
+#     def __init__(self):
+#         self.str1 = ""
+#     def get_String(self)   :
+#         self.str1 = input("Enter string") 
+#     def print_String(self)    :
+#         print("The result is :",self.str1.upper())
+# str1 = Iostring()        
+# str1.get_String()
+# str1.print_String()
+
+# class Employee:
+#     def __init__(self):
+#         print("employee")
+#     def __del__(self)   :
+#         print("Destructor called") 
+# def Create_obj()    :
+#     print("MAKING OBJECT ......................................")    
+#     obj = Employee()
+#     print("FUNCTION end")
+#     return obj
+# print("Create object function...................................................................................................................................................................................................................................................................................")
+# obj = Create_obj()
+# print("program end...........................................................................................................................................................................................................................................................................................................................................................................")
+
+str = "RISHIK PATHAK"
+for index,i in enumerate(str):
+    print(index,i)
