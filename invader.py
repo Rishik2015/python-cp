d955ca6fbc414b80d67c288a6b1058bc40670178
Rishik2015 <panchalminu@gmail.com>
2026-06-02 14:15:30 +0530

    initial commit

--- invader.py
+++ invader.py
@@ -26,10 +26,35 @@ e_x = []
 e_y = []
 e_x_change = []
 e_y_change = []
-numofenemies = 6
+numofenemies = 3
 for i in range(numofenemies):
     e_img.append(pygame.image.load('enamy.png'))
     e_x.append(random.randint(0,sc_wid -64))
     e_y . append(random.randint(en_st_y_min, en_st_y_max))
     e_x_change.append(en_sp_x)
     e_y_change.append(en_sp_y)
+bulletimg =   pygame.image.load('bullet.png')
+bx = 0
+by = pl_st_y
+by_change = bul_sp_y
+bullet_state = 'ready'
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf',32)
+ty = 10
+tx = 10
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+def show_score(x,y):
+    score =  font.render("score : " + str(score_value),True,(255,255,255))
+    screen.blit(score(x,y))
+def game_over_text()    :
+    over_text = over_font.render("game over you lost sillygoose", True,(255,57,67))
+    screen.blit(over_text,28,254)
+def player(x,y)    :
+    screen.blit(playerimg,(x,y))
+def enemy(x,y,i)    :
+        screen.blit(e_img[i],(x,y))
+def fire_bullet(x,y)        :
+     global bullet_state
+     bullet_state = "fire"
+     screen.blit(bulletimg,(x + 16 , y+ 10))
+def
